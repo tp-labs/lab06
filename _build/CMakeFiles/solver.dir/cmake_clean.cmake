4de3f5c63c85b23f5a7946aeac2e3ab5d594acad
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-03-26 21:05:56 +0300

    dfg

---
+++ _build/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/solver_application/equation.cpp.o"
+  "CMakeFiles/solver.dir/solver_application/equation.cpp.o.d"
+  "solver"
+  "solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CPackConfig.cmake
@@ -0,0 +1,72 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "/Users/ksenia55/labor6;/Users/ksenia55/labor6/_build")
+set(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEBIAN_FILE_NAME "solver-.deb")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "SodAcetate")
+set(CPACK_DEBIAN_PACKAGE_NAME "solverapp-dev")
+set(CPACK_DEBIAN_PACKAGE_RELEASE "1")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "/usr/local/Cellar/cmake/3.25.2/share/cmake/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "solver built using CMake")
+set(CPACK_DMG_SLA_USE_RESOURCE_FILE_LICENSE "ON")
+set(CPACK_GENERATOR "DEB")
+set(CPACK_GENERATOR "DEB")
+set(CPACK_INSTALL_CMAKE_PROJECTS "/Users/ksenia55/labor6/_build;solver;ALL;/")
+set(CPACK_INSTALL_PREFIX "/usr/local")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "solver 0.1.1")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+set(CPACK_NSIS_PACKAGE_NAME "solver 0.1.1")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OBJDUMP_EXECUTABLE "/Library/Developer/CommandLineTools/usr/bin/objdump")
+set(CPACK_OSX_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk")
+set(CPACK_OUTPUT_CONFIG_FILE "/Users/ksenia55/labor6/_build/CPackConfig.cmake")
+set(CPACK_PACKAGE_CONTACT "sodium.acetate.82@gmail.com")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "/Users/ksenia55/labor6/DESCRIPTION")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ app for solving quadratic equations")
+set(CPACK_PACKAGE_FILE_NAME "solver-0.1.1-Darwin")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "solver 0.1.1")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "solver 0.1.1")
+set(CPACK_PACKAGE_NAME "solver")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Humanity")
+set(CPACK_PACKAGE_VERSION "0.1.1")
+set(CPACK_PACKAGE_VERSION_MAJOR "0")
+set(CPACK_PACKAGE_VERSION_MINOR "1")
+set(CPACK_PACKAGE_VERSION_PATCH "1")
+set(CPACK_RESOURCE_FILE_LICENSE "/Users/ksenia55/labor6/LICENSE")
+set(CPACK_RESOURCE_FILE_README "/Users/ksenia55/labor6/README.md")
+set(CPACK_RESOURCE_FILE_WELCOME "/usr/local/Cellar/cmake/3.25.2/share/cmake/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+set(CPACK_SOURCE_IGNORE_FILES "\\.cmake;/build/;/.git/;/.github/")
+set(CPACK_SOURCE_INSTALLED_DIRECTORIES "/Users/ksenia55/labor6; /")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/Users/ksenia55/labor6/_build/CPackSourceConfig.cmake")
+set(CPACK_SYSTEM_NAME "Darwin")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "Darwin")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "/Users/ksenia55/labor6/_build/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
