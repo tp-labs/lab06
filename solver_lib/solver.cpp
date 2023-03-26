09a5937fb7bdf5f7e66a554f7021d34bbabce398
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-03-26 18:52:20 +0300

    extra super puper mega new

---
+++ solver_lib/solver.cpp
@@ -0,0 +1,18 @@
+
+#include "solver.h"
+
+#include <cmath>
+#include <stdexcept>
+
+void solve(float a, float b, float c, float& x1, float& x2)
+{
+    float d = (b * b) - (4 * a * c);
+
+    if (d < 0)
+    {
+        throw std::logic_error{"error: discriminant < 0"};
+    }
+
+    x1 = (-b - sqrt(d)) / (2 * a);
+    x2 = (-b + sqrt(d)) / (2 * a);
+}
